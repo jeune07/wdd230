260ce507d7e30d7f1a41442d975a9ddbc74613b9
jeune07 <jeunewinsley9@gmail.com>
2023-09-29 01:25:40 -0300

    hamburger menu added

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,12 @@ year = currentDate.getFullYear()
 document.getElementById("year").innerText=year
 
 document.getElementById("lastModified").innerText=document.lastModified
+
+const menuBtn = document.querySelector(".hamburger");
+const menuNav = document.querySelector(".mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("is-active");
+    menuNav.classList.toggle("is-active");
+
+});
