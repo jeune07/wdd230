106836d15adf8a34fd13a11b7d90c64c686560f6
jeune07 <jeunewinsley9@gmail.com>
2023-10-14 23:23:35 -0300

    home page ready

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -23,3 +23,13 @@ removeActiveClass.forEach(a => a.addEventListener("click",()=>{
     ulContainer.classList.remove("active");
     container.classList.remove("active");
 }))
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
