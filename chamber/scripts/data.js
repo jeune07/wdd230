2d528519d34f9ac1beeeba29b69e2dd0041e47cd
jeune07 <jeunewinsley9@gmail.com>
2023-10-22 18:29:31 -0300

    compagny directory

--- chamber/scripts/data.js
+++ chamber/scripts/data.js
@@ -49,3 +49,63 @@ function showSlides(n) {
 //get the time form the form
 
 document.getElementById("timestamp").value = new Date().toLocaleString();
+
+
+//directory
+
+const fetchingData =async()=>{
+  const response =await fetch("../data/members.json")
+  try {
+    if (!response.ok){
+      throw new Error("something bag happened");
+    }
+    jsonData =await response.json()
+
+    document.addEventListener("DOMContentLoaded", function () { 
+
+      const cardContainer = document.getElementById("card-container");
+    
+      jsonData.members.forEach((company) => {
+          const card = document.createElement("div");
+          card.classList.add("card");
+    
+          const image = document.createElement("img");
+          image.src = company.image;
+          image.alt = company.name;
+    
+          const name = document.createElement("h2");
+          name.textContent = company.name;
+    
+          const address = document.createElement("p");
+          address.textContent = `Address: ${company.address}`;
+    
+          const phone = document.createElement("p");
+          phone.textContent = `Phone: ${company.phone}`;
+    
+          const website = document.createElement("a");
+          website.href = company.website;
+          website.textContent = "Website";
+    
+          const membershipLevel = document.createElement("p");
+          membershipLevel.textContent = `Membership Level: ${company.membershipLevel}`;
+    
+          const otherInfo = document.createElement("p");
+          otherInfo.textContent = `Other Info: ${company.otherInfo}`;
+    
+          card.appendChild(image);
+          card.appendChild(name);
+          card.appendChild(address);
+          card.appendChild(phone);
+          card.appendChild(website);
+          card.appendChild(membershipLevel);
+          card.appendChild(otherInfo);
+    
+          cardContainer.appendChild(card);
+      });
+    });
+    
+    
+  } catch (error) {
+    console.error("errro", error)
+  }
+}
